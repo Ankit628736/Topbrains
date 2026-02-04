b58adc4ea54f6a46cb9eb3595711933ec1158e88
Ankit Raj <115811457+Ankit628736@users.noreply.github.com>
2026-02-04 11:29:45 +0530

    Restore Swapping.cs with implemented methods

--- Dictionary_Lookup/Swapping.cs
+++ Dictionary_Lookup/Swapping.cs
@@ -1,26 +1,26 @@
-// using System;
-// class Program
-// {
-//     public static void Method1(ref int a,ref int b)
-//     {
-//         int t = a;
-//         a = b;
-//         b = t;
-//     }
-//     public static void Method2(out int a,out int b,int x,int y)
-//     {
-//         a = y;
-//         b = x;
-//     }
-//     static void Main(string[] args)
-//     {
-//         int a = 5;
-//         int b = 3;
-//         int x = a,y = b;
-//         Console.WriteLine($"Before Any Method: {a} | {b}");
-//         Method1(ref a,ref b);
-//         Console.WriteLine($"After Method1: {a} | {b}");
-//         Method2(out a,out b,x,y);
-//         Console.WriteLine($"After Method2: {a} | {b}");
-//     }
-// }
+using System;
+class Program
+{
+    public static void Method1(ref int a,ref int b)
+    {
+        int t = a;
+        a = b;
+        b = t;
+    }
+    public static void Method2(out int a,out int b,int x,int y)
+    {
+        a = y;
+        b = x;
+    }
+    static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 3;
+        int x = a,y = b;
+        Console.WriteLine($"Before Any Method: {a} | {b}");
+        Method1(ref a,ref b);
+        Console.WriteLine($"After Method1: {a} | {b}");
+        Method2(out a,out b,x,y);
+        Console.WriteLine($"After Method2: {a} | {b}");
+    }
+}
