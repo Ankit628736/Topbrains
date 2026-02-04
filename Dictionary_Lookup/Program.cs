ecd0a9b043682c83946113bac9a314d158c6fecf
Ankit Raj <115811457+Ankit628736@users.noreply.github.com>
2026-02-04 11:29:10 +0530

    Uncommented and restored Program.cs functionality

--- Dictionary_Lookup/Program.cs
+++ Dictionary_Lookup/Program.cs
@@ -1,21 +1,21 @@
-﻿// using System;
-// using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Dictionary<int, int> Employees = new Dictionary<int, int>
-//         {
-//             { 1, 2000 },
-//             { 2, 3000 },
-//             { 3, 4000 },
-//         };
-//         int Total = 0;
-//         foreach(var it in Employees)
-//         {
-//             Total += it.Value;
-//         }
-//         Console.WriteLine($"{Total}");
-//     }
-// }
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, int> Employees = new Dictionary<int, int>
+        {
+            { 1, 2000 },
+            { 2, 3000 },
+            { 3, 4000 },
+        };
+        int Total = 0;
+        foreach(var it in Employees)
+        {
+            Total += it.Value;
+        }
+        Console.WriteLine($"{Total}");
+    }
+}
